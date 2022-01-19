f351b5056a65703e1f7a95d907cbb53da01660b0
bossjoker1 <1397157763@qq.com>
2022-01-19 23:20:48 +0800

    day19 唉

---
+++ pythonAlgorithm/Practice/219存在重复元素II.py
@@ -0,0 +1,24 @@
+# hash表存
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        pos = {}
+        for i, num in enumerate(nums):
+            if num in pos and i - pos[num] <= k:
+                return True
+            pos[num] = i
+        return False
+    
+# hash表只维护k大小的窗口
+# 时间复杂度因为有删除添加操作，还会增加
+# 空间复杂度取决于K
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        s = set()
+        for i, num in enumerate(nums):
+            if i > k:
+                s.remove(nums[i - k - 1])
+            if num in s:
+                return True
+            s.add(num)
+        return False
