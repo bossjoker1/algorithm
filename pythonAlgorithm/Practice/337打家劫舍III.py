c82b4b6ecf336cbc247445777cd63b34eb6ddbb9
bossjoker1 <1397157763@qq.com>
2022-02-01 22:58:45 +0800

    大年初一

---
+++ pythonAlgorithm/Practice/337打家劫舍III.py
@@ -0,0 +1,17 @@
+# 每次选择偷或不偷
+# 通过一个列表维护上层偷或不偷的结果
+
+class Solution:
+    def rob(self, root: TreeNode) -> int:
+        def _rob(root):
+            if not root: return 0, 0  # 偷，不偷
+
+            left = _rob(root.left)
+            right = _rob(root.right)
+            # 偷当前节点, 则左右子树都不能偷
+            v1 = root.val + left[1] + right[1]
+            # 不偷当前节点, 则取左右子树中最大的值
+            v2 = max(left) + max(right)
+            return v1, v2
+
+        return max(_rob(root))
