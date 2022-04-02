db8bd872e9150feb21c23eeb4f37e49c799a8506
bossjoker1 <1397157763@qq.com>
2022-04-02 22:01:21 +0800

    4.2

---
+++ pythonAlgorithm/Practice/894所有可能的满二叉树.py
@@ -0,0 +1,20 @@
+# 子问题 => 子树
+# 记忆化搜索：可用cache或者字典
+class Solution(object):
+    memo = {0: [], 1: [TreeNode(0)]}
+
+    def allPossibleFBT(self, N):
+        if N not in Solution.memo:
+            ans = []
+            for x in range(N):
+                y = N - 1 - x
+                for left in self.allPossibleFBT(x):
+                    for right in self.allPossibleFBT(y):
+                        # 从下往上构建
+                        bns = TreeNode(0)
+                        bns.left = left
+                        bns.right = right
+                        ans.append(bns)
+            Solution.memo[N] = ans
+
+        return Solution.memo[N]
