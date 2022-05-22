bce8b0140080fdf48cdb3c88823a576f7c6aa134
bossjoker1 <1397157763@qq.com>
2022-05-22 23:51:54 +0800

    5.22

---
+++ pythonAlgorithm/Practice/94二叉树的中序遍历.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# 非递归
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+
+            cur = stack[-1]
+            stack.pop()
+            res.append(cur.val)
+            root = cur.right
+
+        return res
